e3c105a827c79c3412deac33572dfd8c6ba8cd27
admin <andrei.oskilko69@gmail.com>
2024-07-11 09:13:01 +0300

    «3.2. Intents» изменения  разметке и в структуре кода

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -15,13 +15,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val viewModel: PostViewModel by viewModels()
-        val adapter = PostsAdapter(object : OnInteractionListener {
-            val editPostLauncher =
-                registerForActivityResult(EditPostResultContract()) { editedPost ->
-                    editedPost ?: return@registerForActivityResult
-                    viewModel.save(editedPost)
-                }
 
+        val newPostLauncher = registerForActivityResult(NewPostResultContract()) { result ->
+            result ?: return@registerForActivityResult
+            viewModel.changeContent(result)
+            val newPost = Post(
+                id = 0L,
+                author = "Me",
+                content = viewModel.edited.value?.content ?: "",
+                published = "now",
+            )
+            viewModel.save(newPost)
+        }
+
+        val editPostLauncher = registerForActivityResult(EditPostResultContract()) { editedPost ->
+            editedPost ?: return@registerForActivityResult
+            viewModel.save(editedPost)
+        }
+
+        val adapter = PostsAdapter(object : OnInteractionListener {
             override fun onEdit(post: Post) {
                 editPostLauncher.launch(post)
             }
@@ -40,9 +52,7 @@ class MainActivity : AppCompatActivity() {
                     putExtra(Intent.EXTRA_TEXT, post.content)
                     type = "text/plain"
                 }
-
-                val shareIntent =
-                    Intent.createChooser(intent, getString(R.string.chooser_share_post))
+                val shareIntent = Intent.createChooser(intent, getString(R.string.chooser_share_post))
                 startActivity(shareIntent)
             }
 
@@ -56,83 +66,6 @@ class MainActivity : AppCompatActivity() {
         binding.list.adapter = adapter
         viewModel.data.observe(this) { posts ->
             adapter.submitList(posts)
-        }
-
-        //  viewModel.edited.observe(this) { post ->
-        //      if (post.id == 0L) {
-        //          binding.cancelButton.visibility = View.GONE
-        //          binding.previewText.visibility = View.GONE
-        //          binding.edit.visibility = View.GONE
-        //          binding.editorText.visibility = View.GONE
-        //          binding.content.setText("")
-        //          binding.content.clearFocus()
-        //          return@observe
-        //      }
-        //      binding.cancelButton.visibility = View.VISIBLE
-        //      binding.previewText.visibility = View.VISIBLE
-        //      binding.edit.visibility = View.VISIBLE
-        //      binding.editorText.visibility = View.VISIBLE
-        //      with(binding.content) {
-        //          requestFocus()
-        //          setText(post.content)
-        //      }
-        //  }
-
-        //  binding.save.setOnClickListener {
-        //      with(binding.content) {
-        //          if (text.isNullOrBlank()) {
-        //              Toast.makeText(
-        //                  this@MainActivity,
-        //                  "Поле не может быть пустым",
-        //                  Toast.LENGTH_SHORT
-        //              ).show()
-        //              return@setOnClickListener
-        //          }
-
-        //         viewModel.changeContent(text.toString())
-        //         viewModel.save()
-//
-        //         setText("")
-        //         clearFocus()
-        //         AndroidUtil.hideKeyboard(this)
-        //     }
-        // }
-
-        //binding.cancelButton.setOnClickListener {
-        //    viewModel.cancelEditing()
-        //    with(binding.content) {
-        //        setText("")
-        //        clearFocus()
-        //        AndroidUtil.hideKeyboard(this)
-        //    }
-        //}
-
-        //binding.content.addTextChangedListener(object : TextWatcher {
-        //    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-        //        // Не используется
-        //    }
-//
-        //    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-        //        // Не используется
-        //    }
-//
-        //    override fun afterTextChanged(s: Editable?) {
-        //        val firstLine = s?.toString()?.lines()?.firstOrNull() ?: ""
-        //        binding.previewText.text = firstLine
-        //    }
-        //})
-
-
-        val newPostLauncher = registerForActivityResult(NewPostResultContract()) { result ->
-            result ?: return@registerForActivityResult
-            viewModel.changeContent(result)
-            val newPost = Post(
-                id = 0L,
-                author = "Me",
-                content = viewModel.edited.value?.content ?: "",
-                published = "now",
-            )
-            viewModel.save(newPost)
         }
 
         binding.fab.setOnClickListener {
